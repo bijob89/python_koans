f9b42a434f384fb389169016a6f1654f687b30b0
Bijo Babu <bij.babu@gmail.com>
2017-02-08 10:57:44 +0530

    Lesson 25 completed

--- python2/koans/about_dice_project.py
+++ python2/koans/about_dice_project.py
@@ -17,7 +17,12 @@ class DiceSet(object):
     def roll(self, n):
         # Needs implementing!
         # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        self._values = []
+        for num in range(0,n):
+            a = random.randint(1, 6)
+            self._values.append(a)
+
+
 
 
 class AboutDiceProject(Koan):
